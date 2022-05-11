8f3e0613576f691425f8eee8073074df4ed47b60
양태웅 <didxodnd0422@gmail.com>
2022-05-11 10:58:31 +0900

    Apply kotlin-dsl

---
+++ DailyPoem/buildSrc/src/main/kotlin/Dependency.kt
@@ -0,0 +1,26 @@
+object Dependency {
+
+    object Kotlin {
+        const val SDK = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.21"
+    }
+
+    object AndroidX {
+        const val MATERIAL = "com.google.android.material:material:1.5.0"
+        const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.3"
+        const val APP_COMPAT = "androidx.appcompat:appcompat:1.3.1"
+    }
+
+    object KTX {
+        const val CORE = "androidx.core:core-ktx:1.7.0"
+    }
+
+    object Test {
+        const val JUNIT = "junit:junit:4.13.2"
+        const val ANDROID_JUNIT_RUNNER = "AndroidJUnitRunner"
+    }
+
+    object AndroidTest {
+        const val TEST_RUNNER = "androidx.test.ext:junit:1.1.3"
+        const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.4.0"
+    }
+}
