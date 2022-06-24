154ceddced5bc424125651af08076b0d0b736e42
양태웅 <didxodnd0422@gmail.com>
2022-06-25 01:57:10 +0900

    Implement copy button

--- app/src/main/java/com/patrick/dailypoem/ui/MainActivity.kt
+++ app/src/main/java/com/patrick/dailypoem/ui/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.patrick.dailypoem.ui
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.snackbar.Snackbar
 import com.patrick.dailypoem.R
 import com.patrick.dailypoem.databinding.ActivityMainBinding
 
@@ -13,6 +18,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) = with(binding) {
         super.onCreate(savedInstanceState)
 
+        buttonCopy.setOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            clipboard.setPrimaryClip(ClipData.newPlainText("Copied Poem", textPoemBody.text))
+            Snackbar.make(it, "시가 복사되었습니다", Snackbar.LENGTH_SHORT).setAction("확인"){}.show()
+        }
         buttonShare.setOnClickListener {
             ShareBottomSheet().show(supportFragmentManager, null)
         }
