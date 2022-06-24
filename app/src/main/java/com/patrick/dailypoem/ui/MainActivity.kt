6ba6564883e9f260d97780517ed3f50e068950a6
양태웅 <didxodnd0422@gmail.com>
2022-06-25 00:28:20 +0900

    chore : Move MainActivity into ui package

--- app/src/main/java/com/patrick/dailypoem/ui/MainActivity.kt
+++ app/src/main/java/com/patrick/dailypoem/ui/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.patrick.dailypoem
+package com.patrick.dailypoem.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.patrick.dailypoem.R
 import com.patrick.dailypoem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_main) }
+
     override fun onCreate(savedInstanceState: Bundle?) = with(binding) {
         super.onCreate(savedInstanceState)
 
