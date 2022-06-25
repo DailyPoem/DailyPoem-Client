94fb2c46ca1d7c03ab58a38d8c06b6711056fedd
양태웅 <didxodnd0422@gmail.com>
2022-06-26 01:09:09 +0900

    Apply DataBinding

--- app/src/main/java/com/patrick/dailypoem/ui/MainActivity.kt
+++ app/src/main/java/com/patrick/dailypoem/ui/MainActivity.kt
@@ -5,11 +5,13 @@ import android.content.ClipboardManager
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.snackbar.Snackbar
 import com.patrick.dailypoem.R
@@ -23,20 +25,12 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_main) }
     private val mainViewModel: MainViewModel by viewModels()
 
+    var isLoading: MutableLiveData<Boolean> = MutableLiveData(false)
+
     override fun onCreate(savedInstanceState: Bundle?) = with(binding) {
         super.onCreate(savedInstanceState)
-
-        buttonCopy.setOnClickListener {
-            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            clipboard.setPrimaryClip(ClipData.newPlainText("Copied Poem", textPoemBody.text))
-
-            Snackbar.make(it, "시가 복사되었습니다", Snackbar.LENGTH_SHORT)
-                .setAction("확인") {}
-                .show()
-        }
-        buttonShare.setOnClickListener {
-            ShareBottomSheet().show(supportFragmentManager, null)
-        }
+        activity = this@MainActivity
+        lifecycleOwner = this@MainActivity
 
         mainViewModel.getPoem()
         mainViewModel.poemResult.observe(this@MainActivity) { poemResult ->
@@ -44,33 +38,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun copyPoemToClipboard() {
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val poem = binding.textPoemBody.text
+
+        clipboard.setPrimaryClip(ClipData.newPlainText("Copied Poem", poem))
+
+        Snackbar.make(binding.root, "시가 복사되었습니다", Snackbar.LENGTH_SHORT)
+            .setAction("확인") {}
+            .show()
+    }
+
+    fun showShareBottomSheet() {
+        ShareBottomSheet().show(supportFragmentManager, null)
+    }
+
     private fun handlePoemResult(poemResult: NetworkResult<Poem>) {
         when (poemResult) {
             is NetworkResult.Success -> {
-                isLoading(false)
+                isLoading.value = false
                 // TODO: 요청 성공 시 동작 구현 필요
             }
             is NetworkResult.Error -> {
-                isLoading(false)
+                isLoading.value = false
                 // TODO: 요청 실패 시 동작 구현 필요
             }
             is NetworkResult.Loading -> {
-                isLoading(true)
+                isLoading.value = true
             }
         }
-    }
 
-    private fun isLoading(isLoading: Boolean): Unit = with(binding) {
-        if (isLoading) {
-            progressBar.visibility = VISIBLE
-            buttonCopy.isEnabled = false
-            buttonShare.isEnabled = false
-            buttonSettings.isEnabled = false
-        } else {
-            progressBar.visibility = GONE
-            buttonCopy.isEnabled = true
-            buttonShare.isEnabled = true
-            buttonSettings.isEnabled = true
-        }
     }
 }
