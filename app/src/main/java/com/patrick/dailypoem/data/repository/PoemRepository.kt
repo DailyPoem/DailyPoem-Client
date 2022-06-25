986fe74e3087e3196be842b30b55c0d71e13173e
양태웅 <didxodnd0422@gmail.com>
2022-06-25 22:49:39 +0900

    Add PoemRepository

---
+++ app/src/main/java/com/patrick/dailypoem/data/repository/PoemRepository.kt
@@ -0,0 +1,23 @@
+package com.patrick.dailypoem.data.repository
+
+import com.patrick.dailypoem.data.model.Poem
+import com.patrick.dailypoem.data.network.PoemService
+import com.patrick.dailypoem.util.NetworkResult
+import retrofit2.Response
+import javax.inject.Inject
+
+class PoemRepository @Inject constructor(
+    private val poemService: PoemService
+) {
+    suspend fun getPoem(): NetworkResult<Poem> = try {
+        val result = poemService.getPoem()
+
+        if (result.isSuccessful && result.body() != null) {
+            NetworkResult.Success(result.body()!!)
+        } else {
+            NetworkResult.Error(result.message())
+        }
+    } catch (e: Exception) {
+        NetworkResult.Error(e.stackTraceToString())
+    }
+}
