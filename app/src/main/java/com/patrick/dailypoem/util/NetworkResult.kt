f10dc3b5d9346579d8bcfa2dc966a82e506c2d5b
양태웅 <didxodnd0422@gmail.com>
2022-06-26 00:23:56 +0900

    style : Remove blanks

--- app/src/main/java/com/patrick/dailypoem/util/NetworkResult.kt
+++ app/src/main/java/com/patrick/dailypoem/util/NetworkResult.kt
@@ -5,8 +5,6 @@ sealed class NetworkResult<T>(
     val message: String? = null
 ) {
     class Success<T>(data: T): NetworkResult<T>(data)
-
     class Error<T>(message: String?, data: T? = null): NetworkResult<T>(data, message)
-
     class Loading<T>: NetworkResult<T>()
 }
