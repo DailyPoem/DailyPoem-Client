fa383d1771a9e27cf3631fbf7e232c853092c723
양태웅 <didxodnd0422@gmail.com>
2022-06-25 22:53:33 +0900

    Add MainViewModel

--- app/src/main/java/com/patrick/dailypoem/ui/MainActivity.kt
+++ app/src/main/java/com/patrick/dailypoem/ui/MainActivity.kt
@@ -6,14 +6,18 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.snackbar.Snackbar
 import com.patrick.dailypoem.R
 import com.patrick.dailypoem.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_main) }
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) = with(binding) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/patrick/dailypoem/ui/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.patrick.dailypoem.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.patrick.dailypoem.data.model.Poem
+import com.patrick.dailypoem.data.repository.PoemRepository
+import com.patrick.dailypoem.util.NetworkResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val poemRepository: PoemRepository
+): ViewModel() {
+    private val _poemResult: MutableLiveData<NetworkResult<Poem>> = MutableLiveData(NetworkResult.Loading())
+    val poemResult: LiveData<NetworkResult<Poem>> get() = _poemResult
+
+    fun getPoem() {
+        viewModelScope.launch {
+            _poemResult.value = poemRepository.getPoem()
+        }
+    }
+}
