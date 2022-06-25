89be7895dd935237aee047c0e87bf5ed36c235b6
양태웅 <didxodnd0422@gmail.com>
2022-06-25 22:33:53 +0900

    Add PoemService

---
+++ app/src/main/java/com/patrick/dailypoem/data/network/PoemService.kt
@@ -0,0 +1,18 @@
+package com.patrick.dailypoem.data.network
+
+import com.patrick.dailypoem.data.model.Poem
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface PoemService {
+    /**
+     * 서버에 Poem를 요청
+     *
+     * GET의 엔드포인트는 실제 서버 완성 시 수정해야 합니다
+     *
+     * @return 생성된 Poem을 반환합니다.
+     */
+    // TODO: 서버 완성 시 엔드포인트 수정 필요
+    @GET("/poem")
+    suspend fun getPoem(): Response<Poem>
+}
