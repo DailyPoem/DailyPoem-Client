2d368e9345b32fc748d869fa0272db095359ac32
ckrudals <2004ckck@naver.com>
2022-06-28 10:44:40 +0900

    feat : Add onClick

--- app/src/main/java/com/patrick/dailypoem/ui/ShareBottomSheet.kt
+++ app/src/main/java/com/patrick/dailypoem/ui/ShareBottomSheet.kt
@@ -1,15 +1,25 @@
 package com.patrick.dailypoem.ui
 
+import android.content.ActivityNotFoundException
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.kakao.sdk.common.util.KakaoCustomTabsClient
+import com.kakao.sdk.link.LinkClient
+import com.kakao.sdk.link.WebSharerClient
+import com.kakao.sdk.template.model.*
 import com.patrick.dailypoem.R
 import com.patrick.dailypoem.databinding.FragmentShareBottomSheetBinding
+import timber.log.Timber
 
 class ShareBottomSheet : BottomSheetDialogFragment() {
-    private val binding: FragmentShareBottomSheetBinding by lazy { FragmentShareBottomSheetBinding.inflate(layoutInflater) }
+    private val binding: FragmentShareBottomSheetBinding by lazy {
+        FragmentShareBottomSheetBinding.inflate(
+            layoutInflater
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +31,86 @@ class ShareBottomSheet : BottomSheetDialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = with(binding) {
+        this.view = this@ShareBottomSheet
         buttonShareKakaoTalk.setOnClickListener {
             dismiss()
         }
 
         return root
+    }
+
+    // todo 안의 내용 변경해야함
+    private fun defaultFeed() = FeedTemplate(
+        content = Content(
+            title = "딸기 치즈 케익",
+            description = "#케익 #딸기 #삼평동 #카페 #분위기 #소개팅",
+            imageUrl = "http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+            link = Link(
+                webUrl = "https://developers.kakao.com",
+                mobileWebUrl = "https://developers.kakao.com"
+            )
+        ),
+        social = Social(
+            likeCount = 286,
+            commentCount = 45,
+            sharedCount = 845
+        ),
+        buttons = listOf(
+            Button(
+                "웹으로 보기",
+                Link(
+                    webUrl = "https://developers.kakao.com",
+                    mobileWebUrl = "https://developers.kakao.com"
+                )
+            ),
+            Button(
+                "앱으로 보기",
+                Link(
+                    androidExecParams = mapOf("key1" to "value1", "key2" to "value2"),
+                    iosExecParams = mapOf("key1" to "value1", "key2" to "value2")
+                )
+            )
+        )
+    )
+
+    fun sendKakaoLink() {
+        if (LinkClient.instance.isKakaoLinkAvailable(this.requireContext())) {
+            // 카카오톡으로 카카오링크 공유 가능
+            LinkClient.instance.defaultTemplate(
+                this.requireContext(),
+                defaultFeed()
+            ) { linkResult, error ->
+                if (error != null) {
+                    Timber.e(error, "카카오링크 보내기 실패")
+                } else if (linkResult != null) {
+                    Timber.d("카카오링크 보내기 성공 ${linkResult.intent}")
+                    startActivity(linkResult.intent)
+
+                    // 카카오링크 보내기에 성공했지만 아래 경고 메시지가 존재할 경우 일부 컨텐츠가 정상 동작하지 않을 수 있습니다.
+                    Timber.w("Warning Msg: ${linkResult.warningMsg}")
+                    Timber.w("Argument Msg: ${linkResult.argumentMsg}")
+                }
+            }
+        } else {
+            // 카카오톡 미설치: 웹 공유 사용 권장
+            // 웹 공유 예시 코드
+            val sharerUrl = WebSharerClient.instance.defaultTemplateUri(defaultFeed())
+
+            // CustomTabs으로 웹 브라우저 열기
+
+            // 1. CustomTabs으로 Chrome 브라우저 열기
+            try {
+                KakaoCustomTabsClient.openWithDefault(this.requireContext(), sharerUrl)
+            } catch (e: UnsupportedOperationException) {
+                // Chrome 브라우저가 없을 때 예외처리
+            }
+
+            // 2. CustomTabs으로 디바이스 기본 브라우저 열기
+            try {
+                KakaoCustomTabsClient.open(this.requireContext(), sharerUrl)
+            } catch (e: ActivityNotFoundException) {
+                // 인터넷 브라우저가 없을 때 예외처리
+            }
+        }
     }
 }
