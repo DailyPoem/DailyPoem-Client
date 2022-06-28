3a9352749d80de6b328ce945e212e8df756ddd15
ckrudals <2004ckck@naver.com>
2022-06-28 10:45:00 +0900

    feat : Add timber setting

--- app/src/main/java/com/patrick/dailypoem/DailyPoemApplication.kt
+++ app/src/main/java/com/patrick/dailypoem/DailyPoemApplication.kt
@@ -2,6 +2,12 @@ package com.patrick.dailypoem
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class DailyPoemApplication : Application()
+class DailyPoemApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
