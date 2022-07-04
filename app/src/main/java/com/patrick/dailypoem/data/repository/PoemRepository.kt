35f97c7cdecae6b52de99d558e44c0e2cb266490
ckrudals <2004ckck@naver.com>
2022-07-04 09:50:13 +0900

    refactor : Update response poem data

--- app/src/main/java/com/patrick/dailypoem/data/repository/PoemRepository.kt
+++ app/src/main/java/com/patrick/dailypoem/data/repository/PoemRepository.kt
@@ -1,6 +1,6 @@
 package com.patrick.dailypoem.data.repository
 
-import com.patrick.dailypoem.data.model.Poem
+import com.patrick.dailypoem.data.model.PoemData
 import com.patrick.dailypoem.data.network.PoemService
 import com.patrick.dailypoem.util.NetworkResult
 import javax.inject.Inject
@@ -8,7 +8,7 @@ import javax.inject.Inject
 class PoemRepository @Inject constructor(
     private val poemService: PoemService
 ) {
-    suspend fun getPoem(): NetworkResult<Poem> = try {
+    suspend fun getPoem(): NetworkResult<PoemData> = try {
         val result = poemService.getPoem()
 
         if (result.isSuccessful && result.body() != null) {
