5bd608456637c9cb12fb11562047974369a823f2
양태웅 <didxodnd0422@gmail.com>
2022-07-06 09:23:06 +0900

    Add descriptions

--- app/src/main/java/com/patrick/dailypoem/data/network/RandomImageService.kt
+++ app/src/main/java/com/patrick/dailypoem/data/network/RandomImageService.kt
@@ -7,6 +7,11 @@ import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface RandomImageService {
+    /**
+     * https://unsplash.com/collections/OjKbQySOz6Q/portraits에서 랜덤한 한 이미지를 가져옵니다.
+     *
+     * @return 이미지 하나를 포함하는 응답을 반환합니다.
+     */
     @GET("photos/random")
     suspend fun getRandomImage(
         @Header("Authorization") authorization: String = "Client-ID HmN9iyUFrqDORpCmKq1zZxl0fm7T_DJ0ATn7O7o32NI",
