a2a73dcf14b373e8a4d4b1024f23da0d94611586
양태웅 <didxodnd0422@gmail.com>
2022-07-06 11:41:44 +0900

    Update CaptureManager

--- app/src/main/java/com/patrick/dailypoem/util/CaptureManager.kt
+++ app/src/main/java/com/patrick/dailypoem/util/CaptureManager.kt
@@ -1,18 +1,22 @@
 package com.patrick.dailypoem.util
 
 import android.app.Activity
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Rect
+import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.os.Handler
 import android.os.Looper
+import android.provider.MediaStore
 import android.util.Log
 import android.view.PixelCopy
 import android.view.View
 import android.view.Window
 import com.patrick.dailypoem.util.Constants.TAG
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.time.LocalDateTime
@@ -21,15 +25,39 @@ import java.time.format.DateTimeFormatter
 /**
  * @see <a href="https://hongdroid.tistory.com/6">[안드로이드/Android] Layout 영역 사진첩 저장 + 미디어 스캔</a>
  * @see <a href="https://stackoverflow.com/a/58315279">Software rendering doesn't support hardware bitmaps</a>
+ * @see <a href="https://hwanine.github.io/android/Share/">Android - 로컬에 있는 이미지를 SNS로 공유하기 (Kotlin)</a>
  */
 class CaptureManager {
     companion object {
-        fun capture(view: View, activity: Activity) {
-            captureView(view, activity.window) {
-                saveBitmap(it)
+        /**
+         * @return 저장된 이미지 경로를 반환
+         */
+        fun capture(view: View, activity: Activity, onCaptureSuccess: (Uri?) -> Unit) {
+            captureView(view, activity.window) { bitmap ->
+                saveBitmap(bitmap)
+                onCaptureSuccess(getImageUri(activity.applicationContext, bitmap))
             }
         }
 
+        /**
+         * @return 이미지의 Uri를 반환
+         */
+        private fun getImageUri(context: Context, inImage: Bitmap): Uri? {
+            val bytes = ByteArrayOutputStream()
+            inImage.compress(Bitmap.CompressFormat.PNG, 100, bytes)
+            val path: String = MediaStore.Images.Media.insertImage(
+                context.contentResolver,
+                inImage,
+                "Title",
+                null
+            )
+            return Uri.parse(path)
+        }
+
+        /**
+         * 로컬에 이미지를 저장합니다.
+         * @return 저장된 이미지 경로를 반환
+         */
         private fun saveBitmap(bitmap: Bitmap) {
             val fos: FileOutputStream
 
@@ -45,8 +73,10 @@ class CaptureManager {
 
             val title = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"))
 
+            val finalPath = "$strPath/$title.png"
+
             try {
-                fos = FileOutputStream("$strPath/$title.png")
+                fos = FileOutputStream(finalPath)
                 bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
                 Log.d(TAG, "capture: Success?")
             } catch (e: Exception) {
